f7176f899e0697a8c52d4e07c8517c72ea2e208d
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-10-14 19:09:43 +0500

    upd 3d model

--- src/views/product/ProductItem.vue
+++ src/views/product/ProductItem.vue
@@ -5,6 +5,7 @@
       alt="A 3D Sofa Model"
       camera-controls
       autoplay
+      class="viewer"
     ></model-viewer>
         </div>
 </template>
@@ -16,6 +17,23 @@ export default {
 </script>
 
 <style>
+.model-view {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-
+.viewer {
+  margin: 0 auto;
+  width: 500px;
+  height: 500px;
+}
+@media screen and (max-width: 567px) {
+  .viewer {
+    width: 50px;
+    height: 350px;
+  }
+}
 </style>
