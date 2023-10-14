69c8007b854b892a76e36d039d8c9f905677af6f
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-10-14 11:26:08 +0500

    add orbit control component?

--- src/router/index.js
+++ src/router/index.js
@@ -17,10 +17,13 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/orbit',
+    name: 'orbit',
+    component: () => import('../views/OrbitControl.vue')
   }
 ]
 
