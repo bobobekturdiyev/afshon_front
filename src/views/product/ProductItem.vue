630ebdece786271372d5d8d1d67258f821568013
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-10-14 18:53:29 +0500

    Add 3d model

--- src/views/product/ProductItem.vue
+++ src/views/product/ProductItem.vue
@@ -1,7 +1,12 @@
 <template>
-        <model-viewer src="/sofa.glb" alt="A 3D Model"></model-viewer>
-
-  
+        <div class="model-view"> 
+            <model-viewer
+      src="/sofa.glb"
+      alt="A 3D Sofa Model"
+      camera-controls
+      autoplay
+    ></model-viewer>
+        </div>
 </template>
 
 <script>
@@ -11,5 +16,6 @@ export default {
 </script>
 
 <style>
+
 
 </style>
