75f7980d4b6f0dc7d67178f2ab7ba37767ef6ff0
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-10-14 19:34:20 +0500

    add link for categories

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -1,46 +1,40 @@
 <template>
-    <div class="categories-parent">
-        <div class="categories-item"   v-for="item in categories" :key="item">
-       
-       
-       <div class="categories-item-img">
-
-           <img :src="item.image" :alt="item.name">
-       </div>
-                <p class="categories-item-title">{{ item.name }}</p>
-        
-        </div>
-    </div>
-  
+  <div class="categories-parent">
+    <router-link
+      :to="'/model/' + index"
+      class="categories-item"
+      v-for="(item, index) in categories"
+      :key="index"
+    >
+      <div class="categories-item-img">
+        <img :src="item.image" :alt="item.name" />
+      </div>
+      <p class="categories-item-title">{{ item.name }}</p>
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            categories: [
-                {
-                    
-                    name: "Anatomiya",
-                    image: "/assets/icons/heart.svg"
-                },
-                {
-                    
-                    name: "Zoologiya",
-                    image: "/assets/icons/heart.svg"
-                },
-                {
-                    
-                    name: "Astronomiya",
-                    image: "/assets/icons/heart.svg"
-                }
-            ]
-        }
-    },
-
-}
+  data() {
+    return {
+      categories: [
+        {
+          name: "Anatomiya",
+          image: "/assets/icons/heart.svg",
+        },
+        {
+          name: "Zoologiya",
+          image: "/assets/icons/heart.svg",
+        },
+        {
+          name: "Astronomiya",
+          image: "/assets/icons/heart.svg",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
